5522be6d3efd8be7fa4ec140a20eac7b6e6f1838
mayureshzende <mayureshzende@gmail.com>
2021-12-11 12:24:17 +0530

    added till 11 dec 2021

--- 8kyu/describeAge.js
+++ 8kyu/describeAge.js
@@ -8,3 +8,9 @@ function describeAge(age) {
     ? str + "adult"
     : str + "elderly";
 }
+
+/* 
+function describeAge(age) {
+  return "You're a(n) " + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly")
+}
+*/
